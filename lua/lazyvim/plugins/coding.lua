3a95baeaeeb86bc2fa28a3bef945834dcb887199
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-30 22:32:25 +0100

    joe: disable mini surround

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -132,6 +132,7 @@ return {
   -- and more.
   {
     "echasnovski/mini.surround",
+    enabled = false,
     keys = function(_, keys)
       -- Populate the keys based on the user's options
       local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
